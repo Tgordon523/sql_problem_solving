8373a9802d3dbe0a1c0d4c1744bcd47c2f722fe8
unknown <tgordon523@gmail.com>
2023-09-12 17:44:36 -0500

    problem line added for immediate delivery

--- Medium/immediate_delivery.py
+++ Medium/immediate_delivery.py
@@ -1,5 +1,14 @@
 import duckdb
 
+"""
+Problem:
+If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+
+The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
+Write a solution to find the percentage of immediate orders in the first orders of all customers
+"""
+
 sql_init = """
 Create table If Not Exists Delivery (delivery_id int, customer_id int, order_date date, customer_pref_delivery_date date);
 Truncate table Delivery;
