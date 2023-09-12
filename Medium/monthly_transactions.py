5190de851df9ee947bd2cb0f8f45ce200458c40d
unknown <tgordon523@gmail.com>
2023-09-11 20:17:27 -0500

    monthly aggs with duckdb

---
+++ Medium/monthly_transactions.py
@@ -0,0 +1,23 @@
+import duckdb
+
+sql_init = """
+CREATE TYPE state AS ENUM ('approved', 'declined');
+Create table If Not Exists Transactions (id int, country varchar,state state, amount int, trans_date date);
+Truncate table Transactions;
+insert into Transactions (id, country, state, amount, trans_date) values (121, 'US', 'approved', 1000, '2018-12-18');
+insert into Transactions (id, country, state, amount, trans_date) values (122, 'US', 'declined', 2000, '2018-12-19');
+insert into Transactions (id, country, state, amount, trans_date) values (123, 'US', 'approved', 2000, '2019-01-01');
+insert into Transactions (id, country, state, amount, trans_date) values (124, 'DE', 'approved', 2000, '2019-01-07');
+"""
+
+# print(duckdb.sql('SELECT 42').show())
+
+r1 = duckdb.sql(sql_init)
+
+duckdb.sql("""SELECT CONCAT(YEAR(trans_date), '-', MONTH(trans_date)) monthly, country, COUNT(*) trans_cnt, SUM(CASE WHEN T.state = 'approved' THEN 1 ELSE 0 END) approved_cnt, SUM(T.amount) total,
+           SUM(CASE WHEN T.state = 'approved' THEN T.amount ELSE 0 END) approved_total
+           FROM Transactions T
+           GROUP BY 
+           CONCAT(YEAR(trans_date), '-', MONTH(trans_date)), country
+             """).show()
+# result = duckdb.sql('SELECT tweet_id FROM Tweets WHERE LEN(content) < 15').fetchall()
