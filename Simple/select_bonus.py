1c47d82e58a2ea431184c9596a35d1a40def8c89
unknown <tgordon523@gmail.com>
2023-09-11 20:34:17 -0500

    clear extra lines

--- Simple/select_bonus.py
+++ Simple/select_bonus.py
@@ -11,13 +11,9 @@ insert into Bonus values (2, '500');
 insert into Bonus values (4, '2000');
 """
 
-# print(duckdb.sql('SELECT 42').show())
-
 r1 = duckdb.sql(sql_init)
 
 duckdb.sql("""SELECT * 
            FROM Employee E
            LEFT JOIN Bonus B ON E.empId=B.empId
            WHERE (B.bonus < 1000 OR B.bonus is NULL)  """).show()
-# result = duckdb.sql('SELECT tweet_id FROM Tweets WHERE LEN(content) < 15').fetchall()
-# print(result)
--- Simple/valid_length.py
+++ Simple/valid_length.py
@@ -4,8 +4,6 @@ sql_init = """
 CREATE table IF NOT EXISTS Tweets (tweet_id integer, content varchar);INSERT INTO Tweets values (1, 'Vote for Biden'); INSERT INTO Tweets values (2, 'Let us make America great again!');
 """
 
-# print(duckdb.sql('SELECT 42').show())
-
 r1 = duckdb.sql(sql_init)
 
 duckdb.sql('SELECT tweet_id FROM Tweets WHERE LEN(content) < 15').show()
