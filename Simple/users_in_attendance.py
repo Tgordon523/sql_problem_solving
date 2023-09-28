81222678820f1c94d46c7a3cefda45cfe32a027f
unknown <tgordon523@gmail.com>
2023-09-27 22:12:33 -0500

    contest attendance solved

--- Simple/users_in_attendance.py
+++ Simple/users_in_attendance.py
@@ -3,15 +3,39 @@ import duckdb
 """
 Problem: 
 
-Valid tweets can only have up to 14 characters
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
 """
 
 sql_init = """
-CREATE table IF NOT EXISTS Tweets (tweet_id integer, content varchar);INSERT INTO Tweets values (1, 'Vote for Biden'); INSERT INTO Tweets values (2, 'Let us make America great again!');
+Create table If Not Exists Users (user_id int, user_name varchar(20));
+Create table If Not Exists Register (contest_id int, user_id int);
+Truncate table Users;
+insert into Users (user_id, user_name) values ('6', 'Alice');
+insert into Users (user_id, user_name) values ('2', 'Bob');
+insert into Users (user_id, user_name) values ('7', 'Alex');
+Truncate table Register;
+insert into Register (contest_id, user_id) values ('215', '6');
+insert into Register (contest_id, user_id) values ('209', '2');
+insert into Register (contest_id, user_id) values ('208', '2');
+insert into Register (contest_id, user_id) values ('210', '6');
+insert into Register (contest_id, user_id) values ('208', '6');
+insert into Register (contest_id, user_id) values ('209', '7');
+insert into Register (contest_id, user_id) values ('209', '6');
+insert into Register (contest_id, user_id) values ('215', '7');
+insert into Register (contest_id, user_id) values ('208', '7');
+insert into Register (contest_id, user_id) values ('210', '2');
+insert into Register (contest_id, user_id) values ('207', '2');
+insert into Register (contest_id, user_id) values ('210', '7');
 """
 
 r1 = duckdb.sql(sql_init)
 
-duckdb.sql('SELECT tweet_id FROM Tweets WHERE LEN(content) < 15').show()
-result = duckdb.sql('SELECT tweet_id FROM Tweets WHERE LEN(content) < 15').fetchall()
-print(result)
+duckdb.sql("""
+           SELECT 
+           contest_id, round((COUNT(user_id) / (SELECT COUNT(DISTINCT user_id) user_id FROM Users)), 2) pcnt_attendance
+           FROM Register R
+           GROUP BY contest_id
+           ORDER BY (COUNT(user_id) / (SELECT COUNT(DISTINCT user_id) user_id FROM Users)) DESC
+           """).show()
