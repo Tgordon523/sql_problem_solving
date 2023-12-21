2c306d444e9df240586d9356e992afdd1d462036
unknown <tgordon523@gmail.com>
2023-12-21 17:20:41 -0600

    quality query complete

---
+++ Simple/quality_of_queries.py
@@ -0,0 +1,40 @@
+import duckdb
+
+"""
+Problem:
+
+We define query quality as:
+
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+
+The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+"""
+
+sql_init = """
+Create table If Not Exists Queries (query_name varchar(30), result varchar(50), position int, rating int);
+Truncate table Queries;
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Golden Retriever', '1', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'German Shepherd', '2', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Mule', '200', '1');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Shirazi', '5', '2');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Siamese', '3', '3');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Sphynx', '7', '4');
+"""
+
+r1 = duckdb.sql(sql_init)
+
+duckdb.sql("""
+           SELECT query_name, 
+           ROUND((SUM(rating / position) / COUNT(*)), 2) quality,
+           ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*)), 2) poor_query_percentage
+           FROM queries
+           GROUP BY query_name
+           """).show()
